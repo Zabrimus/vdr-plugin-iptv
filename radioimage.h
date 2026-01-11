565f5156aa2c989f7d9c3193267fd0745c4f796e
Andreas Baierl <ichgeh@imkreisrum.de>
2026-01-11 11:47:38 +0100

    fix stillpicture creation
    cDevice::StillPicture() expects a PES or TS stream.
    Currently the plugin sends raw ES data.
    Change that by adding a PES header to the mpg file.
    
    Signed-off-by: Andreas Baierl <ichgeh@imkreisrum.de>

--- radioimage.h
+++ radioimage.h
@@ -16,6 +16,7 @@ private:
     bool imageShown;
     void Show(const char *file);
     void send_pes_packet(unsigned char *data, int len, int timestamp);
+    void send_still_picture(unsigned char *data, int len);
 
 protected:
     void Action() override;
